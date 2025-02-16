e6174fb9c1918728843501659bb7547414437bdb
Ajene <ajenec@gmail.com>
2025-02-16 14:50:57 -0500

    changing to game

---
+++ app/src/fetch.js
@@ -0,0 +1,27 @@
+//https://www.dnd5eapi.co/api/
+
+const fetchData = async (url, options = {}) => {
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok)
+      throw new Error(
+        `Fetch failed. ${response.status} ${response.statusText}`
+      );
+
+    const contentType = response.headers.get("content-type");
+    if (contentType === null || !contentType.includes("application/json")) {
+      const textData = await response.text();
+      // Return a "tuple", making error handling on the "receiving" end easier
+      return [textData, null];
+    }
+
+    // Return a "tuple", making error handling on the "receiving" end easier
+    const jsonData = await response.json();
+    return [jsonData, null];
+  } catch (error) {
+    // if there was an error, log it and return a tuple: [data, error]
+    console.error(error.message);
+    return [null, error];
+  }
+};
